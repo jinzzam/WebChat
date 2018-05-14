0b471a3e1d07e9722922e2073e9f86baa4d68b87
jinzzam <firstmind_@naver.com>
2018-05-15 01:48:54 +0900

    javascript 에서 html속성 가져오기

--- routes/chat.js
+++ routes/chat.js
@@ -2,15 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('chat');
+router.get('/', function (req, res, next) {
+    res.render('chat');
 });
 
 router.post('/', function (req, res) {
-    console.log(req.body.mytext);
     res.render('chat');
 })
 module.exports = router;
-
-
-
