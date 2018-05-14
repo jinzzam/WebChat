a38676947c7979b4e1748f6d7eee176a5b209a55
jinzzam <firstmind_@naver.com>
2018-05-15 00:59:31 +0900

    reqair bug

--- routes/login.js
+++ routes/login.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/login', function (req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('login', {title: 'Login'});
 });
 
-router.post('/login', function (req, res, next) {
+router.post('/', function (req, res, next) {
     console.log(req.body);
 
-    var id = req.body.id;
-    var password = req.body.pw;
+    var id = req.body.user_id;
+    var password = req.body.user_id;
 
     console.log(id);
     console.log(password);
