a93c5da90b0c2b9b590cf7603a99ca44f37dee45
jinzzam <firstmind_@naver.com>
2018-05-15 23:29:28 +0900

    insert join info into mydb's user table

---
+++ public/javascripts/dbconnection.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123',
+    port: '3306',
+    database: 'mydb'
+});
+connection.connect();
+
+// connection.query('select * from user', function (err, rows, fields) {
+//     if (!err)
+//         console.log('The solution is ', rows);
+//     else
+//         console.log('Error while performing Query.', err);
+// });
+
+//connection.end();
+
+module.exports = connection;
--- routes/join.js
+++ routes/join.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var connection = require('../public/javascripts/dbconnection');
 var router = express.Router();
 
 /* GET home page. */
@@ -7,7 +8,46 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
+    var name = req.body.name;
+    var birthday = req.body.birthday;
+    var number = req.body.number;
+    var id = req.body.user_id;
+    var password = req.body.user_pw;
+    var verifypw = req.body.verify_pw;
 
-})
+    var insertsql = 'insert into user values(\'' + name + '\', \'' + birthday + '\', \'' + number + '\', \'' + id + '\', \'' + password +'\')';
+
+    connection.query(insertsql, function (err, rows, fields) {
+        if(!err)
+            console.log('The solution is ', rows);
+        else
+            console.log('Error while performing Query.', err);
+
+    });
+
+
+    // var sql = 'select * from user where name=' + name;
+    // connection.query(sql, function (err, rows, fields) {
+    //     if(!err)
+    //         console.log('The solution is ', rows);
+    //     else
+    //         console.log('Error while performing Query.', err);
+    // })
+
+    console.log(req.body);
+    connection.end();
+
+    res.redirect('/join');
+});
 
 module.exports = router;
+
+
+// connection.query('select * from user', function (err, rows, fields) {
+//     if (!err)
+//         console.log('The solution is ', rows);
+//     else
+//         console.log('Error while performing Query.', err);
+// });
+//
+// connection.end();
