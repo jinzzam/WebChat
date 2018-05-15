b37db662ef9b9d4f47b478d5ed772ffae6170189
jinzzam <firstmind_@naver.com>
2018-05-15 23:28:27 +0900

    add body parser

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
+var bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var app = express();
 var io = require('socket.io').listen(4000);
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,6 +24,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -46,6 +51,15 @@ app.use(function (err, req, res, next) {
 });
 
 module.exports = app;
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123',
+    port: '3306',
+    database: 'mydb'
+});
 
 io.sockets.on('connection', function (socket) {
     socket.on('join:room',function (data) {
