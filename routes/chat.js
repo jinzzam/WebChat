f090efa4ec5ca0af1c070adf22695359bfcf42c6
jinzzam <firstmind_@naver.com>
2018-05-25 23:06:22 +0900

    add upload logic

--- routes/chat.js
+++ routes/chat.js
@@ -6,7 +6,15 @@ router.get('/', function (req, res, next) {
     res.render('chat');
 });
 
-router.post('/', function (req, res) {
+router.post('/', function (req, res, next) {
     res.render('chat');
-})
+});
+
+router.post('/:filename', function (req, res, next) {
+    upload(req, res).then(function (file) {
+        res.json(file);
+    }, function (err) {
+        res.send(500, err);
+    });
+});
 module.exports = router;
